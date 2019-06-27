e51c5717e9dcc14f50c7790a1262ac7924ea4b76
Andrei Rabusov <a.rabusov@tum.de>
2019-06-27 11:27:25 +0200

    [LorentzGroup] rotation

--- Lorentz.hs
+++ Lorentz.hs
@@ -6,6 +6,7 @@ module Lorentz where
     newtype HermVector = HermVector {getVector :: Vector (Complex Double)} deriving (Show, Eq)
     toHermVector :: Vector Double -> HermVector
     toHermVector (Vector x y z) = HermVector (Vector (x :+ 0.0) (y :+ 0.0) (z :+ 0.0))
+
     
     data FourVector = FourVector { t :: Complex Double
                              , r :: HermVector
@@ -18,6 +19,10 @@ module Lorentz where
         vmult :: (Complex Double) -> a -> a
         vplus :: a -> a -> a
         ith :: a -> Int -> Complex Double
+        rotateOfZ :: a -> Double -> a
+        rotateOfX :: a -> Double -> a
+        rotateOfY :: a -> Double -> a
+        rho :: a -> Double
     instance CanCdot HermVector where
         (HermVector (Vector x1 y1 z1)) `cdot` (HermVector (Vector x2 y2 z2)) =
             (conjugate (x1) * x2) + (conjugate (y1) * y2) + (conjugate (z1) * z2)
@@ -27,10 +32,24 @@ module Lorentz where
                                           | n == 1  = y
                                           | n == 2  = z
                                           | otherwise = error "index out of range."
+        rotateOfZ (HermVector (Vector x y z)) psi = HermVector  (Vector x' y' z) where
+            x' = x * (cos (psi) :+ 0.0) - y * (sin (psi) :+ 0.0)
+            y' = x * (sin (psi) :+ 0.0) + y * (cos (psi) :+ 0.0)
+        rotateOfX (HermVector (Vector x y z)) psi = HermVector  (Vector x y' z') where
+            y' = y * (cos (psi) :+ 0.0) - z * (sin (psi) :+ 0.0)
+            z' = y * (sin (psi) :+ 0.0) + z * (cos (psi) :+ 0.0)
+        rotateOfY (HermVector (Vector x y z)) psi = HermVector  (Vector x' y z') where
+            z' = z * (cos (psi) :+ 0.0) - x * (sin (psi) :+ 0.0)
+            x' = z * (sin (psi) :+ 0.0) + x * (cos (psi) :+ 0.0)
+        rho hermvec = magnitude (sqrt (hermvec `cdot` hermvec))
     instance CanCdot FourVector where
         (FourVector t1 r1) `vplus` (FourVector t2 r2) = FourVector (t1+t2) (r1 `vplus` r2)
         lambda `vmult` (FourVector t1 r1) = FourVector (lambda*t1) (lambda `vmult` r1)
         (FourVector t1 r1) `cdot` (FourVector t2 r2) = conjugate (t1) * t2 - r1 `cdot` r2
         ith (FourVector t r) n | n == 0 = t
                                | otherwise = ith r (n-1)
+        rotateOfZ (FourVector t r) psi = FourVector t (rotateOfZ r psi)
+        rotateOfX (FourVector t r) psi = FourVector t (rotateOfX r psi)
+        rotateOfY (FourVector t r) psi = FourVector t (rotateOfY r psi)
+        rho (FourVector t r) = rho r
 
--- test.hs
+++ test.hs
@@ -6,27 +6,40 @@ main = do
     let zeroV = HermVector (Vector (0.0:+0.0) (0.0:+0.0) (0.0:+0.0))
     putStr "1: "
     if (a `cdot` a == 54.0:+0)
-        then do print "Ok."
-        else do print "Fail."
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
     putStr "2: "
     if (a `cdot` zeroV == 0.0:+0.0)
-        then do print "Ok."
-        else do print "Fail."
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
     putStr "3: "
     if ( (0.0:+0.0) `vmult` a == zeroV)
-        then do print "Ok."
-        else do print "Fail."
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
     putStr "4: "
     if ( a `vplus` zeroV == a)
-        then do print "Ok."
-        else do print "Fail."
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
     let mpi = 0.139
     let pv = Vector 0.3 0.2 0.5
     let ppi = toFourVector mpi pv
     putStr "5: "
     if magnitude((ppi `cdot` ppi) - (mpi*mpi :+ 0)) < 10.0^^(-16)
-        then do print "Ok."
+        then do putStrLn "Ok."
         else do 
             putStr "Fail. ppi**2 - mpi**2 = "
             print $ ppi `cdot` ppi - (mpi*mpi :+ 0)
+            putStrLn ""
+    putStr "6: "
+    if rho ppi == rho (rotateOfZ ppi 1.0) 
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
+    putStr "7: "
+    if rho ppi == rho (rotateOfX ppi 2.3)
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
+    putStr "8: "
+    if rho ppi == rho (rotateOfY ppi 3.4)
+        then do putStrLn "Ok."
+        else do putStrLn "Fail."
 
